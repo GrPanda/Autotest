580fd4aed80e20c0041c64189aa50252ad02ea04
GrPanda <41330464+GrPanda@users.noreply.github.com>
2018-07-17 16:51:05 +0300

    3 change

--- firsttest/src/test/java/test/autotest/anton/FirstTest.java
+++ firsttest/src/test/java/test/autotest/anton/FirstTest.java
@@ -1,33 +1,35 @@
 package test.autotest.anton;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.assertTrue;
 
 public class FirstTest {
 
+    private static WebDriver driver;
 
+    @BeforeClass
+    public static void setup() {
+            System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+            driver.get("https://ya.ru");
+    }
     @Test
-    public static void yandex() {
-        System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        driver.get("https://ya.ru");
-
+    public  void yandex() {
         WebElement searchInput = driver.findElement(By.id("text"));
         searchInput.sendKeys ("погода пенза");
-
         WebElement loginButton = driver.findElement(By.cssSelector("button"));
         loginButton.click();
-
-        assertTrue(driver.findElement(By.cssSelector("a[href='https://yandex.ru/pogoda/penza?from=serp_title'] div[class='organic__url-text']")).getText().contains("Пензе"));
-
+        String a = " a[href='https://yandex.ru/pogoda/penza?from=serp_title'] div[class='organic__url-text']";
+        assertTrue(driver.findElement(By.cssSelector(a)).getText().contains("Пензе"));
+    }
+    @AfterClass
+    public static void end() {
         driver.quit();
     }
 }
