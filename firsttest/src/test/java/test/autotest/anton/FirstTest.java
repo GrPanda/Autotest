068c640bc1117a132aceb77911e941f57fb1f497
GrPanda <41330464+GrPanda@users.noreply.github.com>
2018-07-19 02:16:51 +0300

    6 change

--- firsttest/src/test/java/test/autotest/anton/FirstTest.java
+++ firsttest/src/test/java/test/autotest/anton/FirstTest.java
@@ -9,9 +9,7 @@ public class FirstTest extends BaseTest {
     @Test
     public  void FirstTest() {
         searchPage.inputSearh("погода пенза");
-
         searchPage.clickSearchButton();
-
         String linkText = searchPage.getLinkText();
 
         assertTrue(linkText.contains("Пензе"));
