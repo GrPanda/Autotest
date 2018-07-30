f24cf31f65c6e2dcf388efa7707cb8c0264dd1c5
GrPanda <41330464+GrPanda@users.noreply.github.com>
2018-07-30 05:41:20 +0300

    Added 5 test and now it work

--- firsttest/src/test/java/test/autotest/anton/pages/MarketPage.java
+++ firsttest/src/test/java/test/autotest/anton/pages/MarketPage.java
@@ -1,9 +1,8 @@
 package test.autotest.anton.pages;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -14,101 +13,142 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MarketPage {
-    public MarketPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
+    public MarketPage ( WebDriver driver ) {
+        PageFactory.initElements ( driver , this );
         this.driver = driver;
     }
+
+    public String classWhenSortedDescByPrice = "n-filter-sorter i-bem n-filter-sorter_js_inited n-filter-sorter_sort_desc n-filter-sorter_state_select";
+    public String classWhenSortedAscByPrice = "n-filter-sorter i-bem n-filter-sorter_js_inited n-filter-sorter_sort_asc n-filter-sorter_state_select";
     public WebDriver driver;
     public ArrayList arrayList;
+    private String classFromSortedPrice;
 
-    private  String classFromSortedPrice;
-
-    @FindBy(css = "[class='link topmenu__link'][href='/catalog/54425?hid=91009&track=menu']")
+    @FindBy ( css = "[class='link topmenu__link'][href='/catalog/54425?hid=91009&track=menu']" )
     private WebElement computersLink;
-
-    @FindBy(css = "[class='link topmenu__subitem'][href='/catalog/54545/list?hid=6427100&track=menuleaf']")
-    public WebElement tabletLink;
-
-    @FindBy(css = "[class='button button_theme_normal button_arrow_down button_size_s select__button i-bem button_js_inited']")
-    public WebElement viewCountButton;
-
-   @FindBy(id = "uniq15327156811847")
- private WebElement countButton12;
-
-    @FindBy(css = "div.n-filter-block_pos_left.i-bem > div:nth-child(3) > a")
-    private WebElement sortedByPrice;
-
-
-    @FindBy(css = "[class='n-snippet-card2__title']")
-    public List<WebElement> countLinks;
-
-    @FindBy(css = "div[class='price']")
-    public List<WebElement> priceLinks;
-
-    @FindBy(css = "[class='n-snippet-list n-snippet-list_type_vertical metrika b-zone b-spy-init i-bem metrika_js_inited snippet-list_js_inited b-spy-init_js_inited b-zone_js_inited'] >:first-child > div.n-snippet-card2__hover >div [class='n-product-toolbar__item link link_theme_minor hint n-user-lists_type_compare i-bem n-user-lists_type_compare_in-list_no n-user-lists_type_compare_js_inited hint_js_inited _popup2-destructor _popup2-destructor_js_inited link_js_inited']")
-
-    private WebElement toCompareLink;
-// "[class='n-snippet-list n-snippet-list_type_vertical metrika b-zone b-spy-init i-bem metrika_js_inited snippet-list_js_inited b-spy-init_js_inited b-zone_js_inited'] >:first-child > div.n-snippet-card2__hover"
-    @FindBy(css = "[class='n-snippet-list n-snippet-list_type_vertical metrika b-zone b-spy-init i-bem metrika_js_inited snippet-list_js_inited b-spy-init_js_inited b-zone_js_inited'] >:first-child > div.n-snippet-card2__hover >div [class='n-product-toolbar__item link link_theme_minor hint n-user-lists_type_compare i-bem n-user-lists_type_compare_in-list_no n-user-lists_type_compare_js_inited hint_js_inited _popup2-destructor _popup2-destructor_js_inited link_js_inited link_hovered_yes']")
+    @FindBy ( css = "[class='link topmenu__subitem'][href='/catalog/54545/list?hid=6427100&track=menuleaf']" )
+    private WebElement tabletLink;
+    @FindBy ( css = "[class='button button_theme_normal button_arrow_down button_size_s select__button i-bem button_js_inited']" )
+    private WebElement viewCountButton;
+    @FindBy ( css = ".select__list .select__item:nth-of-type(1)" )
+    private WebElement countButton12;
+    @FindBy ( css = ".select__list .select__item:nth-of-type(2)" )
+    private WebElement countButton48;
+    @FindBy ( css = "div.n-filter-block_pos_left.i-bem > div:nth-child(3)" )
+    public WebElement sortedByPrice;
+    @FindBy ( css = "[class='n-snippet-card2__title']" )
+    private List <WebElement> itemLinks;
+    @FindBy ( css = "div[class='price']" )
+    private List <WebElement> priceLinks;
+    @FindBy ( css = "[class='layout__col i-bem layout__col_search-results_normal'] > div > div >div >div:last-child   div[class='price']:last-child" )
+    private WebElement lastPriceLink;
+    @FindBy ( css = "div > div:nth-child(2) > div.n-snippet-card2__hover > div > div > div" )
+    private WebElement secondItemForCompare;
+    @FindBy ( css = "div > div:first-child > div.n-snippet-card2__hover > div > div > div" )
+    private WebElement firstItemForCompare;
+    @FindBy ( css = "[href='/compare?track=head']" )
     private WebElement compareLink;
+    @FindBy ( css = ".n-compare-head.i-bem.n-compare-head_js_inited > div > div > div >div" )
+    public List <WebElement> compareItemsList;
+    @FindBy ( css = ".n-compare-head.i-bem.n-compare-head_js_inited > div > div > div >div:first-child > div > span" )
+    private WebElement firstCompareItemDeleteButton;
+    @FindBy ( css = ".n-compare-head.i-bem.n-compare-head_js_inited > div > div > div >div:nth-child(2) > div > span" )
+    private WebElement secondCompareItemDeleteButton;
+    @FindBy ( css = ".n-compare-head.i-bem.n-compare-head_js_inited > div > div > div >div:first-child" )
+    public WebElement firstCompareItem;
+    @FindBy ( css = ".n-compare-head.i-bem.n-compare-head_js_inited > div > div > div >div:nth-child(2)" )
+    public WebElement secondCompareItem;
 
-    public void moveToCompareLink () {
+    public ArrayList <String> obtainedList = new ArrayList <> ( );
+    public ArrayList <String> sortedList = new ArrayList <> ( );
 
-        Actions actions = new Actions(driver);
-        actions.moveToElement(toCompareLink);
-        actions.perform();
-    }
-    public void clickCompareLink() {
-        compareLink.click();
-    }
-
-    public ArrayList<String> obtainedList = new ArrayList<>();
-
-    public ArrayList<String> sortedList = new ArrayList<>();
-
-    public void moveToComputersLink () {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(computersLink);
-        actions.perform();
-    }
-
-    public void clickTabletLink() {
-        tabletLink.click();
+    public void clickFirstCompareItemDeleteButton ( ) {
+        WebElement explicitWait = ( new WebDriverWait ( driver , 10 ) )
+                .until ( ExpectedConditions.elementToBeClickable ( firstCompareItemDeleteButton ) );
+        firstCompareItemDeleteButton.click ( );
     }
 
-  //  public void waitForElement(WebElement locator) {
-      //  (new WebDriverWait(driver, 2))
-    //            .until(ExpectedConditions.presenceOfElementLocated((By) locator));
-   // }
-
-    public void clickViewCountButton() {
-        viewCountButton.click();
+    public void clickSecondCompareItemDeleteButton ( ) {
+        WebElement explicitWait = ( new WebDriverWait ( driver , 10 ) )
+                .until ( ExpectedConditions.elementToBeClickable ( secondCompareItemDeleteButton ) );
+        secondCompareItemDeleteButton.click ( );
     }
 
-    public void moveToComputersLink1 () {
-
-        Actions actions = new Actions(driver);
-        actions.moveToElement(viewCountButton);
-        actions.perform();
+    public void clickCompareLink ( ) {
+        compareLink.click ( );
     }
 
-    public void clickCountButton12() {
-        countButton12.click();
+    public void clickSecondItemForCompare ( ) {
+        secondItemForCompare.click ( );
     }
 
-    public void clicksortedByPrice() {
-        sortedByPrice.click();
+    public void clickFirstItemForCompare ( ) {
+        firstItemForCompare.click ( );
     }
 
-    public void sortByPrice(){
-       obtainedList.clear();
-        for (WebElement we : priceLinks) {
-             obtainedList.add(we.getText());
+    public void moveToCompareIemLink2 ( ) {
+        Actions actions = new Actions ( driver );
+        actions.moveToElement ( secondCompareItemDeleteButton );
+        actions.perform ( );
     }
-       sortedList.clear();
-        for (String s : obtainedList) {
-            sortedList.add(s);
+
+    public void moveToCompareIemLink ( ) {
+        Actions actions = new Actions ( driver );
+        actions.moveToElement ( firstCompareItemDeleteButton );
+        actions.perform ( );
+    }
+
+    public void moveToComputersLink ( ) {
+        Actions actions = new Actions ( driver );
+        actions.moveToElement ( computersLink );
+        actions.perform ( );
+    }
+
+    public void clickViewCountButton ( ) {
+        viewCountButton.click ( );
+    }
+
+    public void clickCountButton12 ( ) {
+        countButton12.click ( );
+    }
+
+    public void clickCountButton48 ( ) {
+        countButton48.click ( );
+    }
+
+    public void sortByPrice ( ) {
+        Boolean explicitWait = ( new WebDriverWait ( driver , 10 ) )
+                .until ( ExpectedConditions.invisibilityOf ( priceLinks.get ( 0 ) ) );
+        obtainedList.clear ( );
+        sortedList.clear ( );
+        priceLinks.clear ( );
+        for ( WebElement we : priceLinks ) {
+            obtainedList.add ( we.getText ( ) );
+        }
+        for ( String s : obtainedList ) {
+            sortedList.add ( s );
         }
     }
 
+    public void clicksortedByPrice ( ) {
+        sortedByPrice.click ( );
+    }
+
+    public void clickTabletLink ( ) {
+        WebElement explicitWait = ( new WebDriverWait ( driver , 10 ) )
+                .until ( ExpectedConditions.elementToBeClickable ( tabletLink ) );
+        tabletLink.click ( );
+    }
+
+    public int getCountItemLinks ( ) {
+        Boolean explicitWait = ( new WebDriverWait ( driver , 10 ) )
+                .until ( ExpectedConditions.invisibilityOf ( itemLinks.get ( 0 ) ) );
+        return itemLinks.size ( );
+    }
+
+    private void clickWhenItGetVisible ( WebElement webElement ) {
+        WebElement explicitWait = ( new WebDriverWait ( driver , 10 ) )
+                .until ( ExpectedConditions.visibilityOf ( webElement ) );
+        webElement.click ( );
+    }
 }
--- firsttest/src/test/java/test/autotest/anton/pages/NavigationPage.java
+++ firsttest/src/test/java/test/autotest/anton/pages/NavigationPage.java
@@ -1,16 +1,10 @@
 package test.autotest.anton.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 
 public class NavigationPage {
@@ -23,52 +17,35 @@ public class NavigationPage {
 
     @FindBy(css ="a[href='https://market.yandex.ru/?clid=505&utm_source=face_abovesearch&utm_campaign=face_abovesearch']")
     private WebElement marketLink;
-
-
-        //4 ЗАДАНИЕ
     @FindBy(css = "[data-id='video']")
     private WebElement videoLink;
-
     @FindBy(css = "[data-id='images']")
     private WebElement imagesLink;
-
     @FindBy(css = "[data-id='news']")
     private WebElement newsLink;
-
     @FindBy(css = "[data-id='maps']")
     private WebElement mapsLink;
-
     @FindBy(css = "[data-id='translate']")
     private WebElement translateLink;
-
     @FindBy(css = "[data-id='music']")
     private WebElement musicLink;
-
     @FindBy(css = "[data-id='tvonline']")
     private WebElement tvonlineLink;
-
     @FindBy(css = "[class='thumb-image__image related-serp__image']")
     public List<WebElement> videoLinks;
-
     @FindBy(css = "[class='image__image']")
     public List<WebElement> imagesLinks;
-
     @FindBy(css = "[class='logo logo_type_link logo_name_news-ru-77x26']")
     public List<WebElement> newsMainLink;
-
     @FindBy(css = "[class='_app_ready _font_loaded _map_ready']")
     public List<WebElement> mapsMainBody;
-
     @FindBy(css = "[class='logo logo_type_link logo_part_market']")
     public List<WebElement> marketMainLink;
-
     @FindBy(id = "fakeArea")
     public List<WebElement> translateInputField;
-
     @FindBy(css = "[class='d-input__field deco-input deco-input_suggest']")
     public List<WebElement> musicInputField;
-
-    @FindBy(css = "[class='stream-watching__player-container']")
+    @FindBy(css = "[class='stream-watching__player-overlay stream-watching__link']")
     public List<WebElement> tvonlineStreamWindow;
 
     public void clickMarketLink() {
@@ -103,9 +80,14 @@ public class NavigationPage {
         tvonlineLink.click();
     }
 
-    public String getUrl() {
-                String url = driver.getCurrentUrl();
-return url;
+    public String getUrl() throws InterruptedException {
+     //  if (tvonlineStreamWindow.size() > 0)
+      //
+       //    String  url = driver.getCurrentUrl();
+       //return url;}
+                     String  url = driver.getCurrentUrl();
+            return url;
+
     }
 }
 
